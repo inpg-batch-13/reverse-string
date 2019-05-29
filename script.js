39e3f989524f7cc8fa40a347d25b651876174ba4
Rahmat Hidayat <kickyou44@gmail.com>
2019-05-29 19:57:27 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function reverseString(text) {
+  // your code here
+}
+
+console.log(reverseString('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(reverseString('John Doe')); // eoD nhoJ
+console.log(reverseString('I am a bookworm')); // mrowkoob a ma I
+console.log(reverseString('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(reverseString('Super')); // repuS
